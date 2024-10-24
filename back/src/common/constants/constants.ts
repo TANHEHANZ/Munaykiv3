cb002299ebd513c80692a84b8a78f5769985d73e
TANHEHANZ <hantach10@gmail.com>
2024-10-24 14:43:10 -0400

    fix: redirect google

--- back/src/common/constants/constants.ts
+++ back/src/common/constants/constants.ts
@@ -1,5 +1,5 @@
 export const GOOGLE_SCOPE = ["profile", "email"];
-export const CALLGOOGLE = "http://localhost:3000/auth/google/callback";
+export const CALLGOOGLE = "https://munaykiv3.vercel.app/auth/google/callback";
 export const CALLFACEBOOK = "http://localhost:3000/auth/facebook/callback";
 export const FACEBOOK_SCOPE = ["email"];
 export const PROFILEFIELDS = ["id", "emails", "name"];
