eaba8841918d95825134a45ac646e2b8ac60c557
TANHEHANZ <hantach10@gmail.com>
2024-10-24 14:45:47 -0400

    add google callback

--- back/dist/common/constants/constants.js
+++ back/dist/common/constants/constants.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PROFILEFIELDS = exports.FACEBOOK_SCOPE = exports.CALLFACEBOOK = exports.CALLGOOGLE = exports.GOOGLE_SCOPE = void 0;
 exports.GOOGLE_SCOPE = ["profile", "email"];
-exports.CALLGOOGLE = "http://localhost:3000/auth/google/callback";
+exports.CALLGOOGLE = "https://munaykiv3.vercel.app/auth/google/callback";
 exports.CALLFACEBOOK = "http://localhost:3000/auth/facebook/callback";
 exports.FACEBOOK_SCOPE = ["email"];
 exports.PROFILEFIELDS = ["id", "emails", "name"];
